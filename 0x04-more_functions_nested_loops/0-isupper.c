bf389f16e93000a7d73eb297f6b7a68a6a1503d3
genoo778 <123953872+genoo778@users.noreply.github.com>
2023-02-24 23:12:53 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,9 @@
 #include "holberton.h"
-/**
- * _isupper - checks for uppercase character
- *@c: int
- *Return: 1 if upper, 0 other
- */
-
-int _isupper(int c)
+int _isupper(int x)
 {
-char alpha;
-for (alpha = 'A'; alpha <= 'Z'; alpha++)
-{
-	if (c == alpha)
-		return (1);
-}
-
-return (0);
+    if(x>=65 && x<=98)
+    {
+	    return(1);
+    }
+	return (0);
 }

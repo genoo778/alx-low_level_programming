3fef0f00db0e4d9a85ed478b6aeea113ef009422
genoo778 <123953872+genoo778@users.noreply.github.com>
2023-02-24 23:26:49 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,29 +1,23 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * more_numbers - entry point
- *
- * Description: Prints the numbers with _putchar
- *
- * Return: void
- */
+ * more_numbers - prints 10 times the numbers, from 0 to 14.
+**/
 
 void more_numbers(void)
 {
-	int number, tens, units, row;
+	int i, j;
 
-	for (row = 1; row <= 10; row++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (number = 0; number <= 14; number++)
+		for (j = 0; j <= 14; j++)
 		{
-			tens = number / 10;
-			units = number % 10;
-			if (number > 9)
-				_putchar(tens + '0');
-
-			_putchar(units + '0');
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
 }
-

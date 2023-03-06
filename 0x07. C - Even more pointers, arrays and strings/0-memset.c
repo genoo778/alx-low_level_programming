5374e4133cd0eb8e37131ccabea0b312f7947a35
genoo778 <123953872+genoo778@users.noreply.github.com>
2023-03-07 01:54:21 +0200

    Update 0-memset.c

--- 0x07. C - Even more pointers, arrays and strings/0-memset.c
+++ 0x07. C - Even more pointers, arrays and strings/0-memset.c
@@ -1,17 +1,21 @@
 #include "holberton.h"
 
 /**
- * _memset - Entry point
- * @s: pointed destination
- * @b: constant byte
- * @n: bytes
- * Return: Always 0 (Success)
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; n > 0; i++, n--)
+	{
 		s[i] = b;
+	}
+
 	return (s);
 }
